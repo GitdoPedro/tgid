c2ec26f50a05ac0115aec1cf7c440678b66f02fb
Pedro Lima <emailpedrolima@gmail.com>
2024-01-15 00:34:52 -0300

    vatry-catch

--- src/main/java/com/example/tgid/service/TransacaoService.java
+++ src/main/java/com/example/tgid/service/TransacaoService.java
@@ -7,6 +7,7 @@ import com.example.tgid.model.Transacao;
 import com.example.tgid.repository.ClienteRepository;
 import com.example.tgid.repository.EmpresaRepository;
 import com.example.tgid.repository.TransacaoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,60 +27,67 @@ public class TransacaoService {
 
 
     public ResponseEntity<String> depositar(Transacao transacao) {
-        Cliente clienteEncontrado = clienteRepository.getReferenceById(transacao.getCliente().getId());
-        Empresa empresaEncontrada = empresaRepository.getReferenceById(transacao.getEmpresa().getId());
+        try {
+            Cliente clienteEncontrado = clienteRepository.getReferenceById(transacao.getCliente().getId());
+            Empresa empresaEncontrada = empresaRepository.getReferenceById(transacao.getEmpresa().getId());
 
-        if(clienteEncontrado == null || empresaEncontrada == null){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Depósito não efetuado. Cliente ou Empresa não encontrados");
+            if (clienteEncontrado == null || empresaEncontrada == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("Depósito não efetuado. Cliente ou Empresa não encontrados");
+            }
+
+            Tarifa tarifa = new Tarifa(transacao.getValor());
+
+            Transacao transacaoSalva = new Transacao();
+            transacaoSalva.setCliente(clienteEncontrado);
+            transacaoSalva.setEmpresa(empresaEncontrada);
+            transacaoSalva.setValor(tarifa.getValorReajustado());
+            transacaoSalva.setDataHora(LocalDateTime.now());
+            empresaEncontrada.setSaldo(empresaEncontrada.getSaldo().add(transacaoSalva.getValor()));
+
+            transacaoRepository.save(transacaoSalva);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body("Depósito efetuado " + transacaoSalva.getValor().toString() + " " +
+                            "na conta da empresa : " + transacaoSalva.getEmpresa().getNome() +
+                            " pelo cliente: " + transacaoSalva.getCliente().getNome());
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Transação não localizada");
         }
-
-        Tarifa tarifa = new Tarifa(transacao.getValor());
-
-        Transacao transacaoSalva = new Transacao();
-        transacaoSalva.setCliente(clienteEncontrado);
-        transacaoSalva.setEmpresa(empresaEncontrada);
-        transacaoSalva.setValor(tarifa.getValorReajustado());
-        transacaoSalva.setDataHora(LocalDateTime.now());
-        empresaEncontrada.setSaldo(empresaEncontrada.getSaldo().add(transacaoSalva.getValor()));
-
-        transacaoRepository.save(transacaoSalva);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body("Depósito efetuado "+ transacaoSalva.getValor().toString()+ " "+
-                        "na conta da empresa : "+transacaoSalva.getEmpresa().getNome()+
-                        " pelo cliente: "+transacaoSalva.getCliente().getNome());
     }
 
     public ResponseEntity<String> sacar(Transacao transacao) {
-        Cliente clienteEncontrado = clienteRepository.getReferenceById(transacao.getCliente().getId());
-        Empresa empresaEncontrada = empresaRepository.getReferenceById(transacao.getEmpresa().getId());
+        try{
+            Cliente clienteEncontrado = clienteRepository.getReferenceById(transacao.getCliente().getId());
+            Empresa empresaEncontrada = empresaRepository.getReferenceById(transacao.getEmpresa().getId());
 
-        if (clienteEncontrado == null || empresaEncontrada == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Depósito não efetuado. Cliente ou Empresa não encontrados");
+            if (clienteEncontrado == null || empresaEncontrada == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("Depósito não efetuado. Cliente ou Empresa não encontrados");
+            }
+
+            if (empresaEncontrada.getSaldo().compareTo(transacao.getValor()) < 0) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("Depósito não efetuado. Saldo insuficiente");
+
+            }
+
+            Tarifa tarifa = new Tarifa(transacao.getValor());
+
+            Transacao transacaoSalva = new Transacao();
+            transacaoSalva.setCliente(clienteEncontrado);
+            transacaoSalva.setEmpresa(empresaEncontrada);
+            transacaoSalva.setValor(tarifa.getValorReajustado().negate());
+            transacaoSalva.setDataHora(LocalDateTime.now());
+            empresaEncontrada.setSaldo(empresaEncontrada.getSaldo().add(transacaoSalva.getValor()));
+
+            transacaoRepository.save(transacaoSalva);
+
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body("Saque efetuado "+ transacaoSalva.getValor().toString()+ " "+
+                            "na conta da empresa : "+transacaoSalva.getEmpresa().getNome()+
+                            " pelo cliente: "+transacaoSalva.getCliente().getNome());
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Transação não localizada");
         }
-
-        if (empresaEncontrada.getSaldo().compareTo(transacao.getValor()) < 0) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Depósito não efetuado. Saldo insuficiente");
-
-        }
-
-        Tarifa tarifa = new Tarifa(transacao.getValor());
-
-        Transacao transacaoSalva = new Transacao();
-        transacaoSalva.setCliente(clienteEncontrado);
-        transacaoSalva.setEmpresa(empresaEncontrada);
-        transacaoSalva.setValor(tarifa.getValorReajustado().negate());
-        transacaoSalva.setDataHora(LocalDateTime.now());
-        empresaEncontrada.setSaldo(empresaEncontrada.getSaldo().add(transacaoSalva.getValor()));
-
-        transacaoRepository.save(transacaoSalva);
-
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body("Saque efetuado "+ transacaoSalva.getValor().toString()+ " "+
-                        "na conta da empresa : "+transacaoSalva.getEmpresa().getNome()+
-                        " pelo cliente: "+transacaoSalva.getCliente().getNome());
-
     }
 }
